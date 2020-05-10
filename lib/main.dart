b5ceea229a88aba594d89610a8139f4dc333b0da
Galby14 <62215962+Galby14@users.noreply.github.com>
2020-05-10 14:33:24 +0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_crud_api_sample_app/home_screen.dart';
+
+
+void main() => runApp(MaterialApp(
+  theme: ThemeData(
+    primaryColor: Colors.grey[400],
+
+  ),
+  home: HomeScreen(),
+));
+
+
